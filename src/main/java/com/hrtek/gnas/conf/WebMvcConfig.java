480cb089cf8640526319a1d6813b89e93f07574c
DaiHuiBlog <d470969047h@163.com>
2019-03-28 11:30:38 +0800

    剔除多余jar

---
+++ src/main/java/com/hrtek/gnas/conf/WebMvcConfig.java
@@ -0,0 +1,22 @@
+//package com.hrtek.gnas.conf;
+//
+//import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+///**
+// * @Author: daihui
+// * @Description:
+// * @Date: Created in 14:12 2018-12-11
+// * @Modified By:
+// */
+//@Configuration
+//public class WebMvcConfig extends WebMvcConfigurerAdapter {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        //将所有/static/** 访问都映射到classpath:/static/ 目录下
+//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/resources/");
+//    }
+//}
